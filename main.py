3c40f55a10490ec3de2538f98cc8b2ce251381d9
Gustavo Ferreira <gustavo.ferreira@ufrgs.br>
2022-07-10 18:45:06 -0300

    funcoes faltantes e adequacoes a especificacao

--- main.py
+++ main.py
@@ -1,57 +1,46 @@
 import copy
+import heapq
 import time
 
-from queue import Queue
-
 TAMANHO = 3
+OBJETIVO = '12345678_'
 
 class Nodo:
-    def __init__(self):
-        self.estado = ''
-        self.pai = None
-        self.acao = ''
-        self.custo = 0
+    def __init__(self, estado, pai, acao, custo):
+        self.estado = estado
+        self.pai = pai
+        self.acao = acao
+        self.custo = custo
 
 
     def expande(self):
-
         lista_nodos = []
-
         sucessores = sucessor(self.estado)
 
         for suc in sucessores:
-            n = Nodo()
-            n.estado = suc[0]
-            n.acao = suc[1]
+            n = Nodo('', None, '', 0)
+            n.estado = suc[1]
+            n.acao = suc[0]
             n.pai = self
             n.custo = self.custo + 1
-
 
             lista_nodos.append(n)
 
         return lista_nodos
-    def caminho(self):
 
-        s = ''
+    def caminho(self):
+        s = []
         while (self.pai != None):
-            s = s + self.acao
+            s.append(self.acao)
             self = self.pai
 
         return s[::-1]
 
 class Moldura:
     def __init__(self):
-        self.lista = [] #[ [1,2,3].[4,5,6],[7,8,' '] ]
-        self.buraco = (2, 2)
-        self.movimentos = ['C','B','D','E']
-        num = 1
-        # for linha in range(0,TAMANHO):
-        #     self.lista.append([])
-        #     for coluna in range(0, TAMANHO):
-        #         self.lista[linha].append(str(num))
-        #         num = num + 1
-        #self.lista[self.buraco[0]][self.buraco[1]] = ' '
-
+        self.lista = [] #[ [1,2,3].[4,5,6],[7,8,'_'] ]
+        self.buraco = (TAMANHO-1, TAMANHO-1)
+        self.movimentos = ['C', 'B', 'D', 'E']
 
     def str_p_lista(self, string): #modifica a lista
         self.lista = []
@@ -60,11 +49,10 @@ class Moldura:
             self.lista.append([])
             for coluna in range(0, TAMANHO):
                 self.lista[linha].append(string[index])
-                if string[index] == ' ':
+                if string[index] == '_':
                     self.buraco = (int(index/TAMANHO), index % TAMANHO)
                 index += 1
         return self.lista
-
 
     def lista_p_string(self): #nao modifica a lista
         aux_moldura = copy.deepcopy(self.lista)
@@ -74,7 +62,6 @@ class Moldura:
             while linha:
                 string = string + str(linha.pop(0))
         return string
-
 
     def troca(self, pos1, pos2):
         aux = self.lista[pos1[0]][pos1[1]]
@@ -120,137 +107,164 @@ class Moldura:
             return 1
         return 0
 
+def expande(nodo): ##mesma função expande() da classe Nodo, em outro escopo, para realizar os testes de testa_solucao
+    lista_nodos = []
+    sucessores = sucessor(nodo.estado)
+
+    for suc in sucessores:
+        n = Nodo('', None, '', 0)
+        n.estado = suc[1]
+        n.acao = suc[0]
+        n.pai = nodo
+        n.custo = nodo.custo + 1
+
+        lista_nodos.append(n)
+
+    return lista_nodos
 
 def sucessor(estado):
     aux_moldura = Moldura()
-
     aux_moldura.str_p_lista(estado)
-    #print(aux_moldura.lista)
     sucessores = []
 
     for direcao in aux_moldura.movimentos:
+        palavra = ''
+        if direcao == 'C':
+            palavra = 'acima'
+        if direcao == 'B':
+            palavra = 'abaixo'
+        if direcao == 'E':
+            palavra = 'esquerda'
+        if direcao == 'D':
+            palavra = 'direita'
+
         if (aux_moldura.mover(direcao) == 1):
-            sucessores.append((aux_moldura.lista_p_string(), direcao))
-        aux_moldura.str_p_lista(estado)
+            sucessores.append((palavra, aux_moldura.lista_p_string()))
+        aux_moldura.str_p_lista(estado) #renova a lista para testar o prox movimento
 
     return sucessores
-
-
-#m = Moldura()
-
-#print(m.str_p_lista('1234 5678'))
-#print(f'buraco:  {m.buraco}')
-#print(sucessor(m.lista_p_string()))
-
-#n = Nodo()
-#n.estado = '2 3541687'
-#n.estado = '12345678 '
-#expandidos =  n.expande()
-
-#for e in expandidos:
-#   print(e.estado, e.custo, e.acao, e.pai.estado)
-
 
 def bfs(estado): #FRONTEIRA = FILA
     explorados = {}
     fronteira = []
-    new = Nodo()
+    new = Nodo('', None, '', 0)
     new.estado = estado
     fronteira.append(new)
 
     while fronteira:
-
-        n = fronteira.pop(0)
-        if n.estado == '12345678 ':
-            break
-            #return n
+        n = fronteira.pop(0) #primeiro q entrou
+        if n.estado == OBJETIVO:
+            print(f'Explorados BFS: {len(explorados)}')
+            return n.caminho()
         if n.estado not in explorados:
             explorados[n.estado] = n
             for vizinho in n.expande():
-                #print(f'vizinho: {vizinho.estado} custo: {vizinho.custo}')
-                #print(f'{len(explorados)} explorados')
-                #print(f'{len(fronteira)} fronteira')
-
                 fronteira.append(vizinho)
-
-
-    return n    #
-
-
-
-
-t = time.time()
-achou = bfs('2 3541687')
-print(achou.estado)
-print(achou.acao)
-print(achou.custo)
-print(achou.pai)
-print(time.time()-t)
-print(achou.caminho())
-
-# s = ''
-# while achou.pai != None:
-#     s = s + achou.acao
-#     achou = achou.pai
-#     print (s[::-1]) ###invertido
-
-
-
-
-# DFS
-
-# 12345678
-# D
-# 68539
-# <__main__.Nodo object at 0x000002199E8AB0D0>
-# 118.71603679656982
-#
-
-
-
-# BFS
-# 12345678
-# D
-# 23
-# <__main__.Nodo object at 0x000001496CA402B0>
-# 191.9502773284912      segundos
-#
-# B
-# BD
-# BDB
-# BDBE
-# BDBEC
-# BDBECD
-# BDBECDC
-# BDBECDCE
-# BDBECDCEB
-# BDBECDCEBE
-# BDBECDCEBEC
-# BDBECDCEBECD
-# BDBECDCEBECDB
-# BDBECDCEBECDBD
-# BDBECDCEBECDBDC
-# BDBECDCEBECDBDCE
-# BDBECDCEBECDBDCEB
-# BDBECDCEBECDBDCEBE
-# BDBECDCEBECDBDCEBEC
-# BDBECDCEBECDBDCEBECD
-# BDBECDCEBECDBDCEBECDB
-# BDBECDCEBECDBDCEBECDBB
-# BDBECDCEBECDBDCEBECDBB
-
-
+    print(f'Explorados BFS: {len(explorados)}')
+    return None
 
 def dfs(estado): #FRONTEIRA = PILHA
-    pass
+    explorados = {}
+    fronteira = []
+    new = Nodo('', None, '', 0)
+    new.estado = estado
+    fronteira.append(new)
+
+    while fronteira:
+        n = fronteira.pop(-1) #ultimo q entrou
+        if n.estado == OBJETIVO:
+            print(f'Explorados DFS: {len(explorados)}')
+            return n.caminho()
+        if n.estado not in explorados:
+            explorados[n.estado] = n
+            for vizinho in n.expande():
+                fronteira.append(vizinho)
+    print(f'Explorados DFS: {len(explorados)}')
+    return None
 
 def astar_hamming(estado): #NUM DE PECAS FORA DO LUGAR
-    pass
+    # h(v) = numero de pecas fora do lugar
+    def h_hamming(estado):
+        h = 0
+        for index, num in enumerate(estado):
+            if not num == OBJETIVO[index]:
+                h += 1
+        #print(h, estado)
+        return h
+
+    i = 0
+    explorados = {}
+    fronteira = []
+    new = Nodo('', None, '', 0)
+    new.estado = estado
+    new.custo = h_hamming(estado)
+    heapq.heappush(fronteira, (new.custo, i, new)) #i = tiebreaker
+
+    while fronteira:
+        n = heapq.heappop(fronteira)[2] #[2] -> posicao do obj Nodo na tupla
+
+        if n.estado == OBJETIVO:
+            print(f'Explorados A* Hamming: {len(explorados)}')
+            return n.caminho()
+        if n.estado not in explorados:
+            explorados[n.estado] = n
+            for vizinho in n.expande():
+                i += 1
+                heapq.heappush(fronteira, (vizinho.custo+h_hamming(vizinho.estado), i, vizinho))
+    print(f'Explorados A* Hamming: {len(explorados)}')
+
+    return None
 
 def astar_manhattan(estado): #SOMA DISTANCIA MANHATTAN
-    pass
 
+    def h_manhattan(estado):
+        h = 0
+        for index, num in enumerate(estado):
+            if not num == OBJETIVO[index]:
+                coord = (int(index/3), index%3)
+                obj_index = OBJETIVO.find(num)
+                obj_coord = (int(obj_index/3), obj_index%3)
+                #print(f'Coord: {coord} Obj_coord: {obj_coord} Num: {num} Obj_find: {obj_index}')
+                h += abs(obj_coord[0]-coord[0]) + abs(obj_coord[1]-coord[1])
+        return h
 
+    i = 0
+    explorados = {}
+    fronteira = []
+    new = Nodo('', None, '', 0)
+    new.estado = estado
+    new.custo = h_manhattan(estado)
+    heapq.heappush(fronteira, (new.custo, i, new)) #i = tiebreaker
 
+    while fronteira:
+        n = heapq.heappop(fronteira)[2] #[2] -> posicao do obj Nodo na tupla
+
+        if n.estado == OBJETIVO:
+            print(f'Explorados A* Manhattan: {len(explorados)}')
+            return n.caminho()
+        if n.estado not in explorados:
+            explorados[n.estado] = n
+            for vizinho in n.expande():
+                i += 1
+                heapq.heappush(fronteira, (vizinho.custo+h_manhattan(vizinho.estado), i, vizinho))
+    print(f'Explorados A* Manhattan: {len(explorados)}')
+
+    return None
+
+##################### TESTES ##########################
+
+t = time.time()
+#achou = astar_hamming('2_3541687')
+#achou = astar_manhattan('2_3541687')
+#achou = bfs('2_3541687')
+achou = dfs('2_3541687')
+#
+#print(f'Caminho: {achou}')
+print(f'Tempo decorrido: {time.time() - t}')
+if achou != None:
+    print(f'Custo: {len(achou)}')
+    #print(achou.pai)
+else:
+    print('Nao achou')
 
 
